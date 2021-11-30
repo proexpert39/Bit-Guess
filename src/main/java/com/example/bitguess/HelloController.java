e985891a8f1417d1af8cdaeed1a14c114bd83cda
buketsoydaner <60819533+buketsoydaner@users.noreply.github.com>
2021-11-30 22:34:33 +0300

    Update HelloController.java

--- src/main/java/com/example/bitguess/HelloController.java
+++ src/main/java/com/example/bitguess/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Btc yükselecek");
+        welcomeText.setText("Btc degısmeyecek");
     }
 }
