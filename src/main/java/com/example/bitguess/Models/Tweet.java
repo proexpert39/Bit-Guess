cbe127a3095c8446737f17794582cf26288e8951
eyuppastirmaci <pastirmacieyup7@gmail.com>
2021-12-05 22:42:37 +0300

    Tweet List Append

---
+++ src/main/java/com/example/bitguess/Models/Tweet.java
@@ -0,0 +1,126 @@
+package com.example.bitguess.Models;
+
+import java.time.LocalDateTime;
+
+public class Tweet {
+
+    private String id;
+    private String user;
+    private String fullName;
+    private String url;
+    private LocalDateTime timeStamp;
+    private int replies;
+    private int likes;
+    private int retweets;
+    private String text;
+    private int Sentiment;
+
+    public Tweet(String id, String user, String fullName, String url, LocalDateTime timeStamp, int replies, int likes, int retweets, String text, int sentiment) {
+        this.id = id;
+        this.user = user;
+        this.fullName = fullName;
+        this.url = url;
+        this.timeStamp = timeStamp;
+        this.replies = replies;
+        this.likes = likes;
+        this.retweets = retweets;
+        this.text = text;
+        Sentiment = sentiment;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getReplies() {
+        return replies;
+    }
+
+    public void setReplies(int replies) {
+        this.replies = replies;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getRetweets() {
+        return retweets;
+    }
+
+    public void setRetweets(int retweets) {
+        this.retweets = retweets;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getSentiment() {
+        return Sentiment;
+    }
+
+    public void setSentiment(int sentiment) {
+        Sentiment = sentiment;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "id=" + id +
+                ", user='" + user + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", url='" + url + '\'' +
+                ", timeStamp=" + timeStamp +
+                ", replies=" + replies +
+                ", likes=" + likes +
+                ", retweets=" + retweets +
+                ", text='" + text + '\'' +
+                ", Sentiment=" + Sentiment +
+                '}';
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.bitguess {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.opencsv;
 
 
     opens com.example.bitguess to javafx.fxml;
