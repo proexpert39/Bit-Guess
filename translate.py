13dbe555211a9982b88030dcf1025366967b3588
proexpert39 <alperenkumcu97@gmail.com>
2022-05-04 14:39:47 +0300

    update column names

--- translate.py
+++ translate.py
@@ -6,21 +6,21 @@ import csv
 
 translator = Translator()
 # open the file in the write mode
-f = open('tweets_10000_20000.csv', 'a')
+f = open('tweets_turkish/bitcoin_tweets_0.csv', 'a')
 writer = csv.writer(f)
-header = ["text","timestamp"]
+header = ["date","text"]
 writer.writerow(header)
 
 
-column_names = ["timestamp","text"]
-with open("tweets.csv", "r",encoding="utf-8") as csvfile:
-    df = read_csv(csvfile,encoding="utf-8",engine='c',usecols=column_names,low_memory=False,sep=";")
+column_names = ["date","text"]
+with open("tweets/bitcoin_tweets_0.csv", "r",encoding="utf-8") as csvfile:
+    df = read_csv(csvfile,encoding="utf-8",engine='c',usecols=column_names,low_memory=False,sep=",")
     for index, row in df.iterrows():
         try:
             if( type(row.text) == str):
                 tweet_text = row.text.strip()
                 turkish_text = translator.translate(tweet_text,dest="tr").text.replace("\n","")  
-                data = [turkish_text,row.timestamp]
+                data = [row.date,turkish_text]
                 writer.writerow(data)
         except Exception as e:
             print(e)
