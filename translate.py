7a4ba21066f09909620e7391e9442684cdbc0b31
proexpert39 <alperenkumcu97@gmail.com>
2022-05-04 14:52:02 +0300

    newline fix

--- translate.py
+++ translate.py
@@ -6,7 +6,7 @@ import csv
 
 translator = Translator()
 # open the file in the write mode
-f = open('tweets_turkish/bitcoin_tweets_0.csv', 'a')
+f = open('tweets_turkish/bitcoin_tweets_0.csv', 'a',newline="",encoding="utf-8")
 writer = csv.writer(f)
 header = ["date","text"]
 writer.writerow(header)
@@ -14,14 +14,15 @@ writer.writerow(header)
 
 column_names = ["date","text"]
 with open("tweets/bitcoin_tweets_0.csv", "r",encoding="utf-8") as csvfile:
-    df = read_csv(csvfile,encoding="utf-8",engine='c',usecols=column_names,low_memory=False,sep=",")
+    df = read_csv(csvfile,encoding="utf-8",engine='c',usecols=column_names,low_memory=False,sep=",",)
     for index, row in df.iterrows():
         try:
             if( type(row.text) == str):
                 tweet_text = row.text.strip()
-                turkish_text = translator.translate(tweet_text,dest="tr").text.replace("\n","")  
-                data = [row.date,turkish_text]
-                writer.writerow(data)
+                turkish_text = translator.translate(tweet_text,dest="tr").text.replace("\r","").replace("\n","")
+                if(turkish_text):
+                    data = [row.date,turkish_text]
+                    writer.writerow(data)
         except Exception as e:
             print(e)
 f.close()
